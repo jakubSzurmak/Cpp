9576a77bd4548d7571a731b678aeaa9c4585cfd2
Kuberos <127786351+jakubSzurmak@users.noreply.github.com>
2023-07-16 19:31:44 +0200

    Delete OOProject directory

--- OOProject/Headers/Antelope.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Animal.h"
-
-class Antelope:public Animal{
- public:
-  Antelope(Organism** boardBeginning, std::vector<Organism*>* deadOrganismBox);
-  char defendAgainst(Organism* attacker) override;
-  Organism* getBaby() override;
-};
